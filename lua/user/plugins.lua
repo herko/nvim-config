24322a09004fe9c3f56a96d898c84bb0f5585a02
Lubomir Herko <lubomir@herko.sk>
2021-12-25 23:27:58 +0100

    Setup autopairs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,6 +72,8 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter", run = ":TSUpdate"
   }
   use "p00f/nvim-ts-rainbow"
+  use "windwp/nvim-autopairs"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
