7577f8834d7253690092559aef8f97e30eb114e4
Lubomir Herko <lubomir@herko.sk>
2021-12-26 22:34:05 +0100

    Setup buffer force close

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -26,6 +26,7 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+keymap("n", "<leader>c", "<cmd>Bdelete!<cr>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
