d97f7e65495b4ba0603f4c6c26d5dec0f3b9206d
Lubomir Herko <lubomir@herko.sk>
2021-12-26 22:23:56 +0100

    Improve neovim startup time

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -85,6 +85,9 @@ return packer.startup(function(use)
   -- LUaLine
   use "nvim-lualine/lualine.nvim"
 
+  -- Startup time improvement
+  use "lewis6991/impatient.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
